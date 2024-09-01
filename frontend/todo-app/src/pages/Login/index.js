79c7fd6ca72ec3bc8962c01f4d0551b745c2c24e
João Guilherme Paiva <38386145+jguilhermepaiva@users.noreply.github.com>
2024-09-01 00:44:08 -0300

    login/register

--- frontend/todo-app/src/pages/Login/index.js
+++ frontend/todo-app/src/pages/Login/index.js
@@ -81,21 +81,25 @@ function Login() {
       setLoading(false);
 
       if (response.ok) {
-        setError("Registration successful! You can now log in.");
+        setError("Registro concluído com sucesso! Faça login para continuar.");
         setIsLogin(true);
       } else {
         const errorData = await response.json();
         if (Array.isArray(errorData.detail)) {
           const errorMessages = errorData.detail.map((err) => err.msg).join(", ");
-          setError(errorMessages || "Registration failed!");
+          setError(errorMessages || "Registro falhou!");
         } else {
-          setError(errorData.detail || "Registration failed!");
+          setError(errorData.detail || "Registro falhou!");
         }
       }
     } catch (error) {
       setLoading(false);
-      setError("An error occurred. Please try again later.");
+      setError("Um erro ocorreu. Por favor tente novamente mais tarde.");
     }
+  };
+
+  const toggleForm = () => {
+    setIsLogin(!isLogin); // Alterna entre true e false
   };
 
   return (
@@ -108,120 +112,138 @@ function Login() {
           <p className="max-md:hidden text-white text-[52px] font-semibold mb-10">
             do it!
           </p>
-          <form
-            className="max-md:flex max-md:flex-col max-md:items-center max-md:justify-center"
-            onSubmit={handleSubmit}
-          >
-            <div>
-              <label className="text-white text-[20px] font-semibold ">
-                Email
-              </label>
-              <br />
-              <input
-                className="bg-[#1E1E26] w-[300px] h-[40px] rounded"
-                style={{
-                  color: "white",
-                  border: "none",
-                  padding: "0 10px",
-                }}
-                type="text"
-                value={loginUsername}
-                onChange={(e) => setLoginUsername(e.target.value)}
-              />
-            </div>
-            <div>
-              <label className="text-white text-[20px] font-semibold mt-3">
-                Senha
-              </label>
-              <br />
-              <input
-                className="bg-[#1E1E26] w-[300px] h-[40px] rounded mb-10"
-                style={{
-                  color: "white",
-                  border: "none",
-                  padding: "0 10px",
-                }}
-                type="password"
-                value={loginPassword}
-                onChange={(e) => setLoginPassword(e.target.value)}
-              />
-            </div>
-            <button
-              style={{
-                color: "#16161C",
-                fontWeight: "600",
-                padding: "10px 100px",
-                width: "300px",
-                border: "none",
-                borderRadius: "5px",
-                background:
-                  "linear-gradient(225deg, #F29682, #EE69AC , #CB4BCF)",
-              }}
-              type="submit"
-              disabled={loading}
-              id="login-form-submit"
+
+          {isLogin ? (
+            <form
+              className="max-md:flex max-md:flex-col max-md:items-center max-md:justify-center"
+              onSubmit={handleSubmit}
             >
-              {loading ? "Logando" : "Continuar"}
-            </button>
-            {error && <p style={{ color: "red" }}>{error}</p>}
-          </form>
-          <form
-            className="hidden max-md:flex max-md:flex-col max-md:items-center max-md:justify-center"
-            onSubmit={handleRegister}
-          >
-            <div>
-              <label className="text-white text-[20px] font-semibold">
-                Nome de Usuário
-              </label>
-              <br />
-              <input
-                className="bg-[#1E1E26] w-[300px] h-[40px] rounded"
+              <div>
+                <label className="text-white text-[20px] font-semibold ">
+                  Email
+                </label>
+                <br />
+                <input
+                  className="bg-[#1E1E26] w-[300px] h-[40px] rounded"
+                  style={{
+                    color: "white",
+                    border: "none",
+                    padding: "0 10px",
+                  }}
+                  type="text"
+                  value={loginUsername}
+                  onChange={(e) => setLoginUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label className="text-white text-[20px] font-semibold mt-3">
+                  Senha
+                </label>
+                <br />
+                <input
+                  className="bg-[#1E1E26] w-[300px] h-[40px] rounded mb-10"
+                  style={{
+                    color: "white",
+                    border: "none",
+                    padding: "0 10px",
+                  }}
+                  type="password"
+                  value={loginPassword}
+                  onChange={(e) => setLoginPassword(e.target.value)}
+                />
+              </div>
+              <button
                 style={{
-                  color: "white",
+                  color: "#16161C",
+                  fontWeight: "600",
+                  padding: "10px 100px",
+                  width: "300px",
                   border: "none",
-                  padding: "0 10px",
+                  borderRadius: "5px",
+                  background:
+                    "linear-gradient(225deg, #F29682, #EE69AC , #CB4BCF)",
                 }}
-                type="text"
-                value={registerUsername}
-                onChange={(e) => setRegisterUsername(e.target.value)}
-              />
-            </div>
+                type="submit"
+                disabled={loading}
+                id="login-form-submit"
+              >
+                {loading ? "Logando" : "Continuar"}
+              </button>
+              {error && <p style={{ color: "red" }}>{error}</p>}
+            </form>
+          ) : (
             <div>
-              <label className="text-white text-[20px] font-semibold mt-3">
-                Senha
-              </label>
-              <br />
-              <input
-                className="bg-[#1E1E26] w-[300px] h-[40px] rounded mb-10"
-                style={{
-                  color: "white",
-                  border: "none",
-                  padding: "0 10px",
-                }}
-                type="password"
-                value={registerPassword}
-                onChange={(e) => setRegisterPassword(e.target.value)}
-              />
-            </div>
-            <button
-              style={{
-                color: "#16161C",
-                fontWeight: "600",
-                padding: "10px 100px",
-                width: "300px",
-                border: "none",
-                borderRadius: "5px",
-                background:
-                  "linear-gradient(225deg, #F29682, #EE69AC , #CB4BCF)",
-              }}
-              type="submit"
-              disabled={loading}
-              id="register-form-submit"
+              <h2 className="text-white text-[24px] font-semibold text-center mb-5">Crie sua conta</h2>
+            <form
+              className="max-md:flex max-md:flex-col max-md:items-center max-md:justify-center"
+              onSubmit={handleRegister}
             >
-              {loading ? "Registrando..." : "Registrar"}
-            </button>
-            {error && <p style={{ color: "red" }}>{error}</p>}
-          </form>
+              <div>
+                <label className="text-white text-[20px] font-semibold">
+                  Nome de Usuário
+                </label>
+                <br />
+                <input
+                  className="bg-[#1E1E26] w-[300px] h-[40px] rounded"
+                  style={{
+                    color: "white",
+                    border: "none",
+                    padding: "0 10px",
+                  }}
+                  type="text"
+                  value={registerUsername}
+                  onChange={(e) => setRegisterUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label className="text-white text-[20px] font-semibold mt-3">
+                  Senha
+                </label>
+                <br />
+                <input
+                  className="bg-[#1E1E26] w-[300px] h-[40px] rounded mb-10"
+                  style={{
+                    color: "white",
+                    border: "none",
+                    padding: "0 10px",
+                  }}
+                  type="password"
+                  value={registerPassword}
+                  onChange={(e) => setRegisterPassword(e.target.value)}
+                />
+              </div>
+              <button
+                style={{
+                  color: "#16161C",
+                  fontWeight: "600",
+                  padding: "10px 100px",
+                  width: "300px",
+                  border: "none",
+                  borderRadius: "5px",
+                  background:
+                    "linear-gradient(225deg, #F29682, #EE69AC , #CB4BCF)",
+                }}
+                type="submit"
+                disabled={loading}
+                id="register-form-submit"
+              >
+                {loading ? "Registrando..." : "Registrar"}
+              </button>
+              {error && <p style={{ color: "red" }}>{error}</p>}
+            </form>
+            </div>
+          )}
+
+          <button 
+            onClick={toggleForm}
+            style={{ 
+              color: "white", 
+              marginTop: "20px", 
+              fontWeight: "600",
+            }}
+          >
+            {isLogin ? "Criar sua Conta" : "Fazer login"}
+          </button>
         </div>
         <div className="max-md:hidden w-2/3 flex items-end justify-end my-20">
           <img className="max-lg:w-[180px]" src={desk_logo} alt="logo" />

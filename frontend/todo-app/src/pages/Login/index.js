d6dbc675090c01e8cdec9b0c84a9c1d30c014b96
João Guilherme Paiva <38386145+jguilhermepaiva@users.noreply.github.com>
2024-08-31 22:30:41 -0300

    registro

--- frontend/todo-app/src/pages/Login/index.js
+++ frontend/todo-app/src/pages/Login/index.js
@@ -4,14 +4,17 @@ import desk_logo from "../../assets/desktop_logo.svg";
 import mobile_logo from "../../assets/mobile_logo.svg";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [isLogin, setIsLogin] = useState(true); 
 
   const navigate = useNavigate();
 
-  const validateForm = () => {
+  const validateForm = (username, password) => {
     if (!username || !password) {
       setError("Username and password are required");
       return false;
@@ -22,12 +25,12 @@ function Login() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (!validateForm()) return;
+    if (!validateForm(loginUsername, loginPassword)) return;
     setLoading(true);
 
     const formDetails = new URLSearchParams();
-    formDetails.append("username", username);
-    formDetails.append("password", password);
+    formDetails.append("username", loginUsername);
+    formDetails.append("password", loginPassword);
 
     try {
       const response = await fetch("http://localhost:8000/token", {
@@ -54,12 +57,53 @@ function Login() {
     }
   };
 
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    if (!validateForm(registerUsername, registerPassword)) return;
+
+    setLoading(true);
+
+    const formDetails = {
+      username: registerUsername,
+      password: registerPassword,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formDetails),
+      });
+
+      setLoading(false);
+
+      if (response.ok) {
+        setError("Registration successful! You can now log in.");
+        setIsLogin(true);
+      } else {
+        const errorData = await response.json();
+        if (Array.isArray(errorData.detail)) {
+          const errorMessages = errorData.detail.map((err) => err.msg).join(", ");
+          setError(errorMessages || "Registration failed!");
+        } else {
+          setError(errorData.detail || "Registration failed!");
+        }
+      }
+    } catch (error) {
+      setLoading(false);
+      setError("An error occurred. Please try again later.");
+    }
+  };
+
   return (
-    <div className="bg-[#16161c] h-[100vh] flex items-center	px-10">
+    <div className="bg-[#16161c] h-[100vh] flex items-center px-10">
       <div className="flex justify-center items-center w-[1200px] m-auto">
         <div className="max-md:flex max-md:flex-col max-md:items-center max-md:justify-center">
           <div className="flex items-start justify-end w-full ">
-            <img className="md:hidden mt-[-150px] mb-20" src={mobile_logo} alt="logo"></img>
+            <img className="md:hidden mt-[-150px] mb-20" src={mobile_logo} alt="logo" />
           </div>
           <p className="max-md:hidden text-white text-[52px] font-semibold mb-10">
             do it!
@@ -74,15 +118,15 @@ function Login() {
               </label>
               <br />
               <input
-                className="bg-[#1E1E26] w-[300px] h-[40px] rounded "
+                className="bg-[#1E1E26] w-[300px] h-[40px] rounded"
                 style={{
                   color: "white",
                   border: "none",
                   padding: "0 10px",
                 }}
                 type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                value={loginUsername}
+                onChange={(e) => setLoginUsername(e.target.value)}
               />
             </div>
             <div>
@@ -98,8 +142,8 @@ function Login() {
                   padding: "0 10px",
                 }}
                 type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={loginPassword}
+                onChange={(e) => setLoginPassword(e.target.value)}
               />
             </div>
             <button
@@ -121,9 +165,66 @@ function Login() {
             </button>
             {error && <p style={{ color: "red" }}>{error}</p>}
           </form>
+          <form
+            className="max-md:flex max-md:flex-col max-md:items-center max-md:justify-center"
+            onSubmit={handleRegister}
+          >
+            <div>
+              <label className="text-white text-[20px] font-semibold">
+                Nome de Usuário
+              </label>
+              <br />
+              <input
+                className="bg-[#1E1E26] w-[300px] h-[40px] rounded"
+                style={{
+                  color: "white",
+                  border: "none",
+                  padding: "0 10px",
+                }}
+                type="text"
+                value={registerUsername}
+                onChange={(e) => setRegisterUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label className="text-white text-[20px] font-semibold mt-3">
+                Senha
+              </label>
+              <br />
+              <input
+                className="bg-[#1E1E26] w-[300px] h-[40px] rounded mb-10"
+                style={{
+                  color: "white",
+                  border: "none",
+                  padding: "0 10px",
+                }}
+                type="password"
+                value={registerPassword}
+                onChange={(e) => setRegisterPassword(e.target.value)}
+              />
+            </div>
+            <button
+              style={{
+                color: "#16161C",
+                fontWeight: "600",
+                padding: "10px 100px",
+                width: "300px",
+                border: "none",
+                borderRadius: "5px",
+                background:
+                  "linear-gradient(225deg, #F29682, #EE69AC , #CB4BCF)",
+              }}
+              type="submit"
+              disabled={loading}
+              id="register-form-submit"
+            >
+              {loading ? "Registrando..." : "Registrar"}
+            </button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+          </form>
         </div>
         <div className="max-md:hidden w-2/3 flex items-end justify-end my-20">
-          <img className="max-lg:w-[180px]" src={desk_logo} alt="logo"></img>
+          <img className="max-lg:w-[180px]" src={desk_logo} alt="logo" />
         </div>
       </div>
     </div>
